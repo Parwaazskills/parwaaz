c1152b5cc69778d816b8a01bedeb2c68608c1756
digittplus <web.digitt@aft.com.pk>
2026-05-11 22:49:21 +0500

    Reskilling

---
+++ app/reskilling/page.tsx
@@ -0,0 +1,20 @@
+
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import ReskillingHero from "../sections/ReskillingHero";
+import ReskillingSubsection from "../sections/Reskillingsubsection";
+import Reskillingservicebreakdown from "../sections/Reskillingservicebreakdown";
+
+export default function Reskilling() {
+  return (
+    <main className="w-full overflow-hidden bg-white">
+        <div className="absolute left-0 top-0 z-[999] w-full">
+                <Navbar />   
+              </div>
+<ReskillingHero/>
+<ReskillingSubsection/>
+<Reskillingservicebreakdown/>
+      <Footer/>
+    </main>
+  );
+}
